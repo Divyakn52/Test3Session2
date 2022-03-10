3e324ec6a9766bad66a97209dcfabdae383e7a6a
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-03-10 09:01:36 +0000

    code completed

--- p2final.c
+++ p2final.c
@@ -1,2 +1,45 @@
-#include<stdio.h>
-
+[2:27 pm, 10/03/2022] Suchaitra Reva: #include<stdio.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef struct _fraction Fraction;
+Fraction input_fraction()
+{
+  Fraction f1;
+  printf("enter fraction in format a b\n");
+  scanf("%d %d",&f1.num,&f1.den);
+  return f1;
+}
+Fraction Smallest_fraction(Fraction f1,Fraction f2,Fraction f3)
+{
+  Fraction smallest;
+  if(f1.num*f2.den<f2.num*f1.den)
+  {
+    if(f1.num*f3.den<f3.num*f1.den)
+    {
+      smallest.num=f1.num;
+      smallest.den=f1.den;
+    }
+    else
+    {
+      smallest.num=f3.num;
+      smallest.den=f3.den;
+    } 
+  }
+  else
+  {
+    if(f2.num*f3.den<f3.num*f2.den)
+    {
+      smallest.num=f2.num;
+      smallest.den=f2.den;
+    }
+    else
+    {
+      smallest.num=f3.num;
+      smallest.den=f3.den;
+    }
+  }
+  return smallest;
+}
+void output(Fraction f…
